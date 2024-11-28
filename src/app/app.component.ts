80fa017c42983a38d967a689679ea1dc1e2bbd87
Ante Coric <ante.coric@involved-it.be>
2024-11-28 23:46:38 +0100

    Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'HackYourFutureTemplate';
+
+  counter = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  name = "Initial name";
 }
